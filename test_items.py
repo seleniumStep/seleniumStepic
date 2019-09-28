624c00f3fc1216ecf6784e346958c212f9628c0e
milko <miklyxomaklain@mail.ru>
2019-09-28 21:38:34 +0300

    Added files for run autotests

---
+++ test_items.py
@@ -0,0 +1,11 @@
+# Задание: запуск автотестов для разных языков интерфейса
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+# Проверка наличия кнопки добавления в корзину
+def test_check_button_basket(browser):
+    browser.get(link)
+    time.sleep(10)
+    assert browser.find_element_by_class_name("btn-add-to-basket"), "Button is not available"
